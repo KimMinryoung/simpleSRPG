dfd5c6c6d1da963ecb67694b6f1d9de1d94a09af
KimMinryoung <9__3_@naver.com>
2016-12-31 21:08:14 +0900

    add atk_range and action

--- main.lua
+++ main.lua
@@ -10,6 +10,9 @@ function love.load(arg)
 	tile_w=35
 	tile_h=33
 
+	temp_x=0
+	temp_y=0
+
 	state=0--0:default 1:pressed a character 2:moved and preparing an action
 
 	player={}
@@ -17,6 +20,17 @@ function love.load(arg)
 	player.speed=6
 	player.x=10
 	player.y=10
+	player.atk_range={
+		{0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0},
+		{0,0,0,1,1,1,0,0,0},
+		{0,0,0,1,0,1,0,0,0},
+		{0,0,0,1,1,1,0,0,0},
+		{0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0}
+	}
 
 	map={
 	   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
@@ -47,6 +61,7 @@ function love.load(arg)
 	love.graphics.setNewFont(12)
 	player.img=love.graphics.newImage("player.png")
 	moveable_layer=love.graphics.newImage("moveable_layer.png")
+	atkable_layer=love.graphics.newImage("atkable_layer.png")
 	button_atk={}
 	button_skill={}
 	button_dfs={}
@@ -67,17 +82,15 @@ function love.load(arg)
 	button_dfs.height=67
 	action_buttons={button_atk,button_skill,button_dfs}
 	moveable = {}
-	for y=1,map_h do
-		moveable[y] = {}
-		for x=1,map_w do
-			moveable[y][x] = 1
-		end
-	end
+	mapstate_clear(moveable,1)
+	atkable = {}
+	mapstate_clear(atkable,0)
 end
 
 function love.draw(dt)
 	draw_map()
 	display_moveable()
+	display_atkable()
 	display_buttons()
 end
 
@@ -102,49 +115,75 @@ function love.keypressed(key, unicode)
 	end
 end
 
+function mouse_to_map_point(mouse_x,mouse_y)
+	x=math.floor((mouse_x-map_offset_x)/tile_w)+map_x
+	y=math.floor((mouse_y-map_offset_y)/tile_h)+map_y
+	return x,y
+end
+
+function move_character(x,y)
+	if x>=1 and x<=map_w and y>=1 and y<=map_h then
+		if moveable[y][x]<=0 then
+			print("moveable point")
+			player.x=x
+			player.y=y
+			mapstate_clear(moveable,1)
+			state=2
+		else
+			print("not moveable point")
+		end
+	end
+end
+
+function action_buttons_click(pos_x,pos_y)
+	for i=1,3 do
+		if between(pos_x,action_buttons[i].x,action_buttons[i].x+action_buttons[i].width) and between(pos_y,action_buttons[i].y,action_buttons[i].y+action_buttons[i].height) then
+			mapstate_clear(moveable,1)
+			if i==1 then
+				state=3
+				atkable_tiles(player.x,player.y,player.atk_range)
+			elseif i==2 then
+				state=4
+			elseif i==3 then
+				state=0
+			end
+		end
+	end
+end
+
+function mapstate_clear(mapstate,num)
+	for y=1,map_h do
+		mapstate[y] = {}
+		for x=1,map_w do
+			mapstate[y][x] = num
+		end
+	end
+end
+
 function love.mousepressed(pos_x, pos_y, button, istouch)
+	x,y=mouse_to_map_point(pos_x,pos_y)
 	if button == 1 then
 		if state==0 then
-			x=math.floor((pos_x-map_offset_x)/tile_w)+map_x
-			y=math.floor((pos_y-map_offset_y)/tile_h)+map_y
 			if player.x==x and player.y==y then
 				moveable_tiles(player.speed,player.x,player.y)
 				state=1
 			end
 		elseif state==1 then
-			x=math.floor((pos_x-map_offset_x)/tile_w)+map_x
-			y=math.floor((pos_y-map_offset_y)/tile_h)+map_y
-			if x>=1 and x<=map_w and y>=1 and y<=map_h then
-				if moveable[y][x]<=0 then
-					print("moveable point")
-					player.x=x
-					player.y=y
-
-					for y=1,map_h do
-						moveable[y] = {}
-						for x=1,map_w do
-							moveable[y][x] = 1
-						end
-					end
-					state=2
-				else
-					print("not moveable point")
-				end
-			end
+			move_character(x,y)
+			action_buttons_click(pos_x,pos_y)
+		elseif state==2 then
+			action_buttons_click(pos_x,pos_y)
 		end
-	end
-	if button == 2 then
+	elseif button == 2 then
 		if state==0 then
 			--do nothing
 			--maybe add option menu later
 		elseif state==1 then
-			for y=1,map_h do
-				moveable[y] = {}
-				for x=1,map_w do
-					moveable[y][x] = 1
-				end
-			end
+			mapstate_clear(moveable,1)
 			state=0
+		elseif state==3 then
+			mapstate_clear(atkable,0)
+			state=2
 		end
 	end
 end
@@ -153,7 +192,7 @@ function draw_map()
 	for y=1, map_display_h do
 		for x=1, map_display_w do
 			love.graphics.draw( 
-			tile[map[y+map_y][x+map_x]], 
+			tile[map[y+map_y][x+map_x]],
 			(x*tile_w)+map_offset_x, 
 			(y*tile_h)+map_offset_y )
 			if y+map_y==player.y and x+map_x==player.x then
@@ -189,15 +228,41 @@ function moveable_tiles(remainStep,x,y)
 	end
 end
 
+
+function atkable_tiles(ctr_x,ctr_y,atk_range)
+	for y=1,9 do
+		for x=1,9 do
+			if atk_range[y][x]==1 and between(ctr_x+(x-5),1,map_w) and between(ctr_y+(y-5),1,map_h) then
+				atkable[ctr_y+(y-5)][ctr_x+(x-5)]=1
+			end
+		end
+	end
+end
+
 function display_moveable()
 	love.graphics.setColor(255, 255, 255, 100)
 	for y=1, map_display_h do
 		for x=1, map_display_w do
 			if(moveable[y+map_y][x+map_x]<=0) then
 				love.graphics.draw( 
-				moveable_layer, 
-				(x*tile_w)+map_offset_x, 
-				(y*tile_h)+map_offset_y )
+					moveable_layer, 
+					(x*tile_w)+map_offset_x, 
+					(y*tile_h)+map_offset_y )
+			end
+		end
+	end
+	love.graphics.setColor(255, 255, 255, 255)
+end
+
+function display_atkable()
+	love.graphics.setColor(255, 255, 255, 150)
+	for y=1, map_display_h do
+		for x=1, map_display_w do
+			if(atkable[y+map_y][x+map_x]==1) then
+				love.graphics.draw( 
+					atkable_layer, 
+					(x*tile_w)+map_offset_x, 
+					(y*tile_h)+map_offset_y )
 			end
 		end
 	end
@@ -213,4 +278,8 @@ function display_buttons()
 				action_buttons[i].y )
 		end
 	end
+end
+
+function between(x,min,max)
+	return x>=min and x<=max
 end
